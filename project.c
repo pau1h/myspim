650baa5e37cb42c9db720f8fe6c55659b5feef07
Paul <pablo@Pauls-MacBook-Pro.local>
2024-04-23 17:13:45 -0400

    refactor instruction_decode

--- project.c
+++ project.c
@@ -1,8 +1,6 @@
 #include "spimcore.h"
 
 
-/* ALU */
-/* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero) //assign 1 to zero if output is 0, else set to 0.
 {
     switch(ALUControl){
@@ -26,8 +24,6 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero) /
     *Zero = (*ALUresult == 0 ? 1 : 0); //if aluresult is zero, set Zero = 1
 }
 
-/* instruction fetch */
-/* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction) //store the instruction in instruction ptr. returns 1 if halt sequence is required.
 {
     if((PC % 4 != 0) || PC < 0 || PC > 0xFFFF){ //halt sequence is reached if PC is out of range of memory, or PC is not word aligned.
@@ -38,8 +34,6 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction) //store t
 }
 
 
-/* instruction partition */
-/* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec) //partition instruction into r1, r2, r3, funct, offset, and jsec
 {
     *op = instruction >> 26;
@@ -57,120 +51,123 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
                                                         op      rs  rt      offset (immediate val)
 
 */
-/* instruction decode */
-/* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
     //for memread, write, or regwrite 0 = disabled, 1 = enabled, 2 = dont care
     //for regdst, jump, branch, memtoreg, or alusrc, the value of 0 or 1 indicates the selected path of the multiplexer; 2 = dont care
-    //TODO: change to switch statement
-    if(op == 0){ //R type instruction
-        controls->RegDst = 1;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls-> MemRead = 0;
-        controls->MemtoReg = 0;
-        controls->ALUOp = 7;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 0;
-        controls->RegWrite = 1;
-    }else if(op == 0x23){ //load word from memory to register
-        controls->RegDst = 0;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->MemRead = 1;
-        controls->MemtoReg = 1;
-        controls->ALUOp = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 1;
-        controls->RegWrite = 1;
-    }else if(op == 0x2B){ //store word from register to mem
-        controls->RegDst = 2;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->MemRead = 0;
-        controls->MemtoReg = 2;
-        controls->ALUOp = 0;
-        controls->MemWrite = 1;
-        controls->ALUSrc = 1;
-        controls->RegWrite = 0;
-    }else if(op == 0x4){ //beq
-        controls->RegDst = 2;
-        controls->Jump = 0;
-        controls->Branch = 1;
-        controls->MemRead = 0;
-        controls->MemtoReg = 2;
-        controls->ALUOp = 1; //aluop set to subtract. if r1 - r2 == 0, registers are equal -> branch
-        controls->MemWrite = 0;
-        controls->ALUSrc = 0;
-        controls->RegWrite = 0;
-    }else if(op == 0xA){ //set less than immediate
-        controls->RegDst = 0;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->MemRead = 0;
-        controls->MemtoReg = 0;
-        controls->ALUOp = 2;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 1;
-        controls->RegWrite = 1;
-    }else if(op == 0xB){ //set less than immediate unsigned 
-        controls->RegDst = 0;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->MemRead = 0;
-        controls->MemtoReg = 0;
-        controls->ALUOp = 3;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 1;
-        controls->RegWrite = 1;
-    }else if(op == 0x8){ //add immediate 
-        controls->RegDst = 0;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->MemRead = 0;
-        controls->MemtoReg = 0;
-        controls->ALUOp = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 1;
-        controls->RegWrite = 1;
-    }else if(op == 0xF){ //load upper immediate. loads constant in upper 16 bits, fills lower 16 bits with 0s
-        controls->RegDst = 0;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->MemRead = 0;
-        controls->MemtoReg = 0;
-        controls->ALUOp = 6; //shift left extended_value by 16 bits
-        controls->MemWrite = 0;
-        controls->ALUSrc = 1;
-        controls->RegWrite = 1;
-    }else if(op == 0x2){ //jump
-        controls->RegDst = 2;
-        controls->Jump = 1;
-        controls->Branch = 2; //DC since jump mux is set to 1
-        controls->MemRead = 0;
-        controls->MemtoReg = 2;
-        controls->ALUOp = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 2;
-        controls->RegWrite = 0;
-    }else{
-        return 1;
+    switch(op){
+        case 0: //R Type instruction
+            controls->RegDst = 1;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls-> MemRead = 0;
+            controls->MemtoReg = 0;
+            controls->ALUOp = 7;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 0;
+            controls->RegWrite = 1;
+            break;
+        case 0x23: //load word from mem to register
+            controls->RegDst = 0;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->MemRead = 1;
+            controls->MemtoReg = 1;
+            controls->ALUOp = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 1;
+            controls->RegWrite = 1;
+            break;
+        case 0x2B: //store word from mem to register
+            controls->RegDst = 2;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->MemRead = 0;
+            controls->MemtoReg = 2;
+            controls->ALUOp = 0;
+            controls->MemWrite = 1;
+            controls->ALUSrc = 1;
+            controls->RegWrite = 0;
+            break;
+        case 0x4: //beq
+            controls->RegDst = 2;
+            controls->Jump = 0;
+            controls->Branch = 1;
+            controls->MemRead = 0;
+            controls->MemtoReg = 2;
+            controls->ALUOp = 1; //aluop set to subtract. if r1 - r2 == 0, registers are equal -> branch
+            controls->MemWrite = 0;
+            controls->ALUSrc = 0;
+            controls->RegWrite = 0;
+            break;
+        case 0xA: //set less than immediate
+            controls->RegDst = 0;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->MemRead = 0;
+            controls->MemtoReg = 0;
+            controls->ALUOp = 2;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 1;
+            controls->RegWrite = 1;
+            break;
+        case 0xB: //set less than immediate unsigned
+            controls->RegDst = 0;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->MemRead = 0;
+            controls->MemtoReg = 0;
+            controls->ALUOp = 3;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 1;
+            controls->RegWrite = 1;
+            break;
+        case 0x8: //add immedaite
+            controls->RegDst = 0;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->MemRead = 0;
+            controls->MemtoReg = 0;
+            controls->ALUOp = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 1;
+            controls->RegWrite = 1;
+            break;
+        case 0xF: //load upper immediate
+            controls->RegDst = 0;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->MemRead = 0;
+            controls->MemtoReg = 0;
+            controls->ALUOp = 6; //shift left extended_value by 16 bits
+            controls->MemWrite = 0;
+            controls->ALUSrc = 1;
+            controls->RegWrite = 1;
+            break;
+        case 0x2: //jump
+            controls->RegDst = 2;
+            controls->Jump = 1;
+            controls->Branch = 2; //DC since jump mux is set to 1
+            controls->MemRead = 0;
+            controls->MemtoReg = 2;
+            controls->ALUOp = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 2;
+            controls->RegWrite = 0;
+            break;
+        default:
+            return 1;
+            break;
     }
     return 0;
 }
 
-/* Read Register */
-/* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2) //read registers addressed by r1 and r2 from Reg. write values to data1 and data2.
 {
     *data1 = Reg[r1];
     *data2 = Reg[r2];
 }
 
-
-/* Sign Extend */
-/* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value) //assign the sign-extended value of offset to extended_value. 16th bit is the sign bit.
 {
     if((offset >> 15) & 0x1){ 
@@ -180,8 +177,6 @@ void sign_extend(unsigned offset,unsigned *extended_value) //assign the sign-ext
     }
 }
 
-/* ALU operations */
-/* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 { 
     unsigned B = (ALUSrc == 1) ? extended_value : data2; //if alusrc is 1, pull data from extended_val instead of reg 2
@@ -206,8 +201,6 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
     return 0;
 }
 
-/* Read / Write Memory */
-/* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem) //use the value of memwrite or read to determine of a mem write / read operation is occuring
 {
     if(MemRead != 0){ //load word from mem to register
@@ -228,8 +221,6 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 }
 
 
-/* Write Register */
-/* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg) //set r2 or r3 based on regdst to aluresult or memdata.
 {
     if(RegWrite == 1){
@@ -237,12 +228,10 @@ void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,
     }
 }
 
-/* PC update */
-/* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC) //update the program counter
 {
      *PC += 4; //update program counter
-     if(Branch == 1 && Zero == 1){ //beq not working, branching to 4030 instead of 4024, its just adding 4 to the PC. BEQ is not being triggered!
+     if(Branch == 1 && Zero == 1){ 
         *PC = *PC + (extended_value<<2);
      }
      if(Jump){
